87f84e8950f1d3419dca6b6a7de4400b7f96ee65
ravi2390 <126110951+ravi2390@users.noreply.github.com>
2025-06-11 23:38:14 -0400

    Refactor table components

---
+++ resources/js/components/Affiliate/Partial/AffiliateDetailDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-toolbar density="compact" elevation="0" class="hidden-lg-and-up mobile-has-selected-row">
+            <v-spacer></v-spacer>
+            <v-btn icon="mdi:mdi-close" @click="$emit('close')" />
+        </v-toolbar>
+        <div class="mobile-selected-item-container">
+            <affiliate-detail-component @swipeToClose="$emit('close')" :affiliateId="affiliateId" :shouldDisplayDataComponent="false" />
+        </div>
+    </div>
+</template>
+<script>
+import AffiliateDetailComponent from './AffiliateDetailComponent';
+export default {
+    name: 'AffiliateDetailDrawer',
+    components: { AffiliateDetailComponent },
+    props: { affiliateId: { type: Number, required: true } }
+};
+</script>
